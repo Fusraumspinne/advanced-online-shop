fc3174189b49c4d448431f6dd20fa537ba892922
Fusraumspinne <mjjaeger2002@gmail.com>
2024-07-09 20:44:50 +0200

    middleware update

--- middleware.js
+++ middleware.js
@@ -1,3 +1,3 @@
 export { default } from "next-auth/middleware"
 
-export const config = { matcher: ["/shop", "/creatordashboard", "/productcreation"] }
+export const config = { matcher: ["/shop", "/creatordashboard", "/productcreation", "/warenkorb", "/product"] }
