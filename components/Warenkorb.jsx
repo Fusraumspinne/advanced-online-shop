3196754152a7423be308beb4cd2824c15863d80f
Fusraumspinne <mjjaeger2002@gmail.com>
2024-07-10 08:57:31 +0200

    preis berechnung warenkorb

--- components/Warenkorb.jsx
+++ components/Warenkorb.jsx
@@ -1,18 +1,25 @@
 "use client"
 
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, use } from 'react'
 import Navigationbar from './ui/Navbar'
 import MagicButton from './ui/Button'
 import Image from 'next/image'
 import { useSession } from "next-auth/react"
 import Link from 'next/link'
 import DeleteIcon from '@mui/icons-material/Delete';
+import Input from './ui/Input'
 
 function Warenkorb() {
   const { data: session } = useSession()
 
   const [email, setEmail] = useState("")
   const [warenkorb, setWarenkorb] = useState([])
+  const [adresse, setAdresse] = useState("")
+
+  const [productPrice, setProductPrice] = useState(0)
+  const [totalItems, setTotalItems] = useState(0)
+  const [lieferKosten, setLieferKosten] = useState(0)
+  const [totalPrice, setTotalPrice] = useState(0)
 
   const fetchWarenkorb = async () => {
     try {
@@ -40,13 +47,30 @@ function Warenkorb() {
   useEffect(() => {
     setEmail(session?.user?.email)
   }, [session])
-  
+
   useEffect(() => {
     fetchWarenkorb()
   }, [email])
 
   useEffect(() => {
     console.log(warenkorb)
+  }, [warenkorb])
+
+  useEffect(() => {
+    const total = warenkorb.reduce((acc, item) => {
+      const formattedPrice = parseFloat(item.preis.replace(/\./g, '').replace(',', '.'));
+      return {
+        price: acc.price + (formattedPrice * item.anzahl),
+        items: acc.items + item.anzahl
+      }
+    }, { price: 0, items: 0 });
+
+    setProductPrice(total.price);
+    setTotalItems(total.items);
+
+    setLieferKosten(total.items * 4)
+
+    setTotalPrice(total.items * 4 + total.price)
   }, [warenkorb])
 
   const handleDelete = async (itemId) => {
@@ -98,7 +122,7 @@ function Warenkorb() {
                     <td><Image src={item.produktBild} alt={item.produktName} width={25} height={25} /></td>
                     <td><Link href={`/product/${item.produkt}`}>{item.produktName}</Link></td>
                     <td>{item.anzahl}</td>
-                    <td>{item.preis}</td>
+                    <td>{item.preis ? `${item.preis}€` : ''}</td>
                     <td><DeleteIcon onClick={() => handleDelete(item._id)} /></td>
                   </tr>
                 ))}
@@ -107,7 +131,14 @@ function Warenkorb() {
           </div>
           <div className='col-4'>
             <form onSubmit={handleSubmit}>
-              <MagicButton content={"Bezahlen"} type={"submit"} />
+              <p>Produktepreis: {productPrice ? productPrice.toLocaleString('de-DE', { minimumFractionDigits: 2, maximumFractionDigits: 2 }) + '€' : '0,00€'}</p>
+              <p>Anzahl: {totalItems ? `${totalItems} Produkte` : '0 Produkte'}</p>
+              <p>Lieferkosten: {lieferKosten ? lieferKosten.toLocaleString('de-DE', { minimumFractionDigits: 2, maximumFractionDigits: 2 }) + '€' : '0,00€'}</p>
+              <p>Gesamtpreis: {totalPrice ? totalPrice.toLocaleString('de-DE', { minimumFractionDigits: 2, maximumFractionDigits: 2 }) + '€' : '0,00€'}</p>
+
+              <Input isLabel={true} contentLabel={"Adresse"} placeholder={"Bülserstraße 80"} onChange={(e) => setAdresse(e.target.value)} />
+
+              <MagicButton content={"Bezahlen"} type={"submit"} extraClass={"full_width_button mt-3"} />
             </form>
           </div>
         </div>
