16ce599c3951630b0b4a67bfd6e9708bcb20417d
Fusraumspinne <mjjaeger2002@gmail.com>
2024-07-27 17:12:20 +0200

    navbar style chnages

--- components/ui/Navbar.jsx
+++ components/ui/Navbar.jsx
@@ -4,14 +4,13 @@ import { signOut } from "next-auth/react"
 
 function Navigationbar() {
     return (
-        <Navbar expand="lg" className="bg-primary" fixed="top">
+        <Navbar expand="lg" className="bg-primary" data-bs-theme="dark" fixed="top">
             <Container fluid>
                 <Navbar.Brand className="text-light fw-bold" href="/">Schnäppchen Safari</Navbar.Brand>
-                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Toggle aria-controls="navbarScroll"/>
                 <Navbar.Collapse id="navbarScroll">
                     <Nav
                         className="me-auto my-2 my-lg-0"
-                        style={{ maxHeight: '100px' }}
                         navbarScroll
                     >
                     <Nav.Link className="text-light" href="/">Shop</Nav.Link>
