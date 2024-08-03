3b1cddf56b329da95324619d33debc353fe8f644
Fusraumspinne <mjjaeger2002@gmail.com>
2024-08-03 10:00:52 +0200

    responsive design anfang

--- components/Productcreation.jsx
+++ components/Productcreation.jsx
@@ -89,47 +89,125 @@ function Productcreation() {
             <Navigationbar />
 
             <div style={{ marginTop: "56px" }}>
-                <div className='row mx-4'>
+                <div className='row mx-4 product_creation'>
                     <div className='col-6' style={{ maxHeight: "calc(100vh - 56px)", overflowY: "auto" }}>
                         <form onSubmit={handleSubmit}>
                             <Input placeholder={"Bastelschere"} type={"text"} onChange={(e) => setProduktName(e.target.value)} isLabel={true} contentLabel={"Produktname"} extraClass={"mt-4"} />
                             <Textarea placeholder={"Scharf Basteln Schere Kinder Werkzeug"} rows={5} onChange={(e) => setStichWörter(e.target.value)} isLabel={true} contentLabel={"Stichwörter"} extraClass={"mt-4"} />
-                            <Input placeholder={"Produktbild"} type={"file"} isLabel={true} contentLabel={"Bild"} extraClass={"mt-4"} onChange={convertToBase64} accept={"image/*"}/>
+                            <Input placeholder={"Produktbild"} type={"file"} isLabel={true} contentLabel={"Bild"} extraClass={"mt-4"} onChange={convertToBase64} accept={"image/*"} />
                             <Input placeholder={"8,99"} type={"text"} onChange={(e) => setPreis(formatPrice(e.target.value))} isLabel={true} contentLabel={"Preis"} extraClass={"mt-4"} />
                             <Textarea placeholder={"Aus hartem Stahl gefertigt um Jahre mit Kindern und harter Benutzung stand halten zu können."} rows={5} onChange={(e) => setAusführlicheBeschreibung(e.target.value)} isLabel={true} contentLabel={"Ausführliche Beschreibung"} extraClass={"mt-4"} />
                             <Input placeholder={"1-2"} type={"text"} onChange={(e) => setLieferzeit(e.target.value)} isLabel={true} contentLabel={"Lieferzeit"} extraClass={"mt-4"} />
 
-                            <div className='row'>
-                                <div className='col-9'>
+                            <div>
+                                <div>
                                     <MagicButton type={"submit"} content={"Create"} extraClass={"mt-4 mb-4 full_width_button"} />
                                 </div>
 
-                                <div className='col-3'>
-                                    {vorschauCard ? (
-                                        <MagicButton content={"Seitenvorschau"} extraClass={"mt-4 mb-4 full_width_button"} funktion={() => setVoraschauCard(false)} />
-                                    ) : (
-                                        <MagicButton content={"Cardvorschau"} extraClass={"mt-4 mb-4 full_width_button"} funktion={() => setVoraschauCard(true)} />
-                                    )}
-                                </div>
                             </div>
                         </form>
+                        <div>
+                            {vorschauCard ? (
+                                <MagicButton content={"Seitenvorschau"} extraClass={"mb-4 full_width_button"} funktion={() => setVoraschauCard(false)} />
+                            ) : (
+                                <MagicButton content={"Cardvorschau"} extraClass={"mb-4 full_width_button"} funktion={() => setVoraschauCard(true)} />
+                            )}
+                        </div>
                     </div>
                     <div className='col-6 d-flex justify-content-center align-items-center'>
                         {vorschauCard ? (
-                            <div>
-                                <Productcard
-                                    produktname={produktName}
-                                    preis={preis}
-                                    stichwörter={stichWörter}
-                                    produktbild={produktBild}
-                                />
+                            <div className={`card mx-2`} style={{ width: '18rem', height: '28rem' }}>
+                                <div style={{ borderRadius: '0.375rem 0.375rem 0 0' }} className='d-flex justify-content-center align-items-center bg-light '>
+                                    <Image style={{ borderRadius: '0.375rem 0.375rem 0 0', width: "100%", maxHeight: "250px", objectFit: "cover" }} src={produktBild} alt={produktBild} width={250} height={250} />
+                                </div>
+                                <div className="card-body">
+                                    <div style={{ height: "50px", overflow: 'hidden', display: '-webkit-box', WebkitBoxOrient: 'vertical', WebkitLineClamp: 2 }}>
+                                        <h1 className="card-title fs-5">{produktName}</h1>
+                                    </div>
+                                    <div className='mt-2' style={{ height: "25px" }}>
+                                        <p className='card-text'>{preis ? `${preis}€` : ''}</p>
+                                    </div>
+                                    <div className='mt-2' style={{ height: "75px", overflow: 'hidden', display: '-webkit-box', WebkitBoxOrient: 'vertical', WebkitLineClamp: 3 }}>
+                                        <p className="card-text">{stichWörter}</p>
+                                    </div>
+                                </div>
                             </div>
                         ) : (
                             <div className='card py-4'>
                                 <div className='row mx-4'>
                                     <div className='col-6' style={{ maxHeight: "calc(100vh - 54px)", overflowY: "auto" }}>
                                         <div className='d-flex justify-content-center' style={{ width: "100%", maxWidth: "200px" }}>
-                                            <Image src={produktBild} alt={produktBild} width={200} height={200} className='card'/>
+                                            <Image src={produktBild} alt={produktBild} width={200} height={200} className='card' />
+                                        </div>
+
+                                        <p style={{ fontSize: "18px" }}>{ausführlicheBeschreibung}</p>
+                                    </div>
+                                    <div className='col-6'>
+                                        <h1 className='fs-2'>{produktName}</h1>
+                                        <p className='fs-4'>{stichWörter}</p>
+
+                                        <p>Verkäufer: {verkäufer}</p>
+                                        <p>Preis: {preis ? `${preis}€` : ''}</p>
+                                        <p>Lieferzeit: {lieferzeit ? `${lieferzeit} Werktage` : ''}</p>
+
+                                        <MagicButton content={"In den Warenkorb"} />
+                                    </div>
+                                </div>
+                            </div>
+                        )}
+
+                    </div>
+                </div>
+
+                <div className='mx-4 product_creation_resposive'>
+                    <div>
+                        <form onSubmit={handleSubmit}>
+                            <Input placeholder={"Bastelschere"} type={"text"} onChange={(e) => setProduktName(e.target.value)} isLabel={true} contentLabel={"Produktname"} extraClass={"mt-4"} />
+                            <Textarea placeholder={"Scharf Basteln Schere Kinder Werkzeug"} rows={5} onChange={(e) => setStichWörter(e.target.value)} isLabel={true} contentLabel={"Stichwörter"} extraClass={"mt-4"} />
+                            <Input placeholder={"Produktbild"} type={"file"} isLabel={true} contentLabel={"Bild"} extraClass={"mt-4"} onChange={convertToBase64} accept={"image/*"} />
+                            <Input placeholder={"8,99"} type={"text"} onChange={(e) => setPreis(formatPrice(e.target.value))} isLabel={true} contentLabel={"Preis"} extraClass={"mt-4"} />
+                            <Textarea placeholder={"Aus hartem Stahl gefertigt um Jahre mit Kindern und harter Benutzung stand halten zu können."} rows={5} onChange={(e) => setAusführlicheBeschreibung(e.target.value)} isLabel={true} contentLabel={"Ausführliche Beschreibung"} extraClass={"mt-4"} />
+                            <Input placeholder={"1-2"} type={"text"} onChange={(e) => setLieferzeit(e.target.value)} isLabel={true} contentLabel={"Lieferzeit"} extraClass={"mt-4"} />
+
+                            <div>
+                                <div>
+                                    <MagicButton type={"submit"} content={"Create"} extraClass={"mt-4 mb-4 full_width_button"} />
+                                </div>
+
+                            </div>
+                        </form>
+                        <div>
+                            {vorschauCard ? (
+                                <MagicButton content={"Seitenvorschau"} extraClass={"mb-4 full_width_button"} funktion={() => setVoraschauCard(false)} />
+                            ) : (
+                                <MagicButton content={"Cardvorschau"} extraClass={"mb-4 full_width_button"} funktion={() => setVoraschauCard(true)} />
+                            )}
+                        </div>
+                    </div>
+                    <div className='d-flex justify-content-center align-items-center mt-4 mb-5'>
+                        {vorschauCard ? (
+                            <div className={`card mx-2`} style={{ width: '18rem', height: '28rem' }}>
+                                <div style={{ borderRadius: '0.375rem 0.375rem 0 0' }} className='d-flex justify-content-center align-items-center bg-light '>
+                                    <Image style={{ borderRadius: '0.375rem 0.375rem 0 0', width: "100%", maxHeight: "250px", objectFit: "cover" }} src={produktBild} alt={produktBild} width={250} height={250} />
+                                </div>
+                                <div className="card-body">
+                                    <div style={{ height: "50px", overflow: 'hidden', display: '-webkit-box', WebkitBoxOrient: 'vertical', WebkitLineClamp: 2 }}>
+                                        <h1 className="card-title fs-5">{produktName}</h1>
+                                    </div>
+                                    <div className='mt-2' style={{ height: "25px" }}>
+                                        <p className='card-text'>{preis ? `${preis}€` : ''}</p>
+                                    </div>
+                                    <div className='mt-2' style={{ height: "75px", overflow: 'hidden', display: '-webkit-box', WebkitBoxOrient: 'vertical', WebkitLineClamp: 3 }}>
+                                        <p className="card-text">{stichWörter}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        ) : (
+                            <div className='card py-4'>
+                                <div className='row mx-4'>
+                                    <div className='col-6' style={{ maxHeight: "calc(100vh - 54px)", overflowY: "auto" }}>
+                                        <div className='d-flex justify-content-center' style={{ width: "100%", maxWidth: "200px" }}>
+                                            <Image src={produktBild} alt={produktBild} width={200} height={200} className='card' />
                                         </div>
 
                                         <p style={{ fontSize: "18px" }}>{ausführlicheBeschreibung}</p>
--- components/ui/Productcard.jsx
+++ components/ui/Productcard.jsx
@@ -3,18 +3,18 @@ import React from 'react'
 
 function Productcard({ produktname, preis, stichwörter, produktbild, extraClass }) {
     return (
-        <div className={`card mx-3 ${extraClass}`}  style={{ width: '18rem', height: '28rem' }}>
+        <div className={`card mx-2 product_card ${extraClass}`}  style={{ width: '18rem', height: '28rem' }}>
             <div style={{ borderRadius: '0.375rem 0.375rem 0 0'}} className='d-flex justify-content-center align-items-center bg-light '>
-                <Image style={{ borderRadius: '0.375rem 0.375rem 0 0', width: "100%", maxHeight: "250px", objectFit: "cover" }} src={produktbild} alt={produktbild} width={250} height={250} />
+                <Image className='product_card_image' style={{ borderRadius: '0.375rem 0.375rem 0 0', width: "100%", maxHeight: "250px", objectFit: "cover" }} src={produktbild} alt={produktbild} width={250} height={250} />
             </div>
             <div className="card-body">
-                <div style={{height: "50px", overflow: 'hidden', display: '-webkit-box', WebkitBoxOrient: 'vertical', WebkitLineClamp: 2}}>
+                <div className='product_card_title' style={{height: "50px", overflow: 'hidden', display: '-webkit-box', WebkitBoxOrient: 'vertical', WebkitLineClamp: 2}}>
                     <h1 className="card-title fs-5">{produktname}</h1>
                 </div>
-                <div className='mt-2' style={{height: "25px"}}>
+                <div className='mt-2 product_card_preis' style={{height: "25px"}}>
                     <p className='card-text'>{preis ? `${preis}€` : ''}</p>
                 </div>
-                <div className='mt-2' style={{height: "75px", overflow: 'hidden', display: '-webkit-box', WebkitBoxOrient: 'vertical', WebkitLineClamp: 3 }}>
+                <div className='mt-2 product_card_stichwörter' style={{height: "75px", overflow: 'hidden', display: '-webkit-box', WebkitBoxOrient: 'vertical', WebkitLineClamp: 3 }}>
                     <p className="card-text">{stichwörter}</p>
                 </div>
             </div>
